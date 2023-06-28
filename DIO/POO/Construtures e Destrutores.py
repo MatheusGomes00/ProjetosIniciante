2e80ee29ad36344b782da5ccaa4f1815e4787467
Matheus gomes <126532702+MatheusGomes00@users.noreply.github.com>
2023-06-28 19:09:00 -0300

    V2-Atualização de comentários

--- DIO/POO/Construtures e Destrutores.py
+++ DIO/POO/Construtures e Destrutores.py
@@ -1,6 +1,65 @@
-# underline alguma coisa underline é um método especial
-# __init__ é um construtor, usado para inicializar valores de um objeto na classe... inicializa a classe
-# __del__ é um destrutor, quando o objeto não está mais sendo usado, podemos 'destruí-lo' para liberar espaço na memória
+'''
+    underline-2x '__' alguma coisa underline-2x '__' é um 
+    método especial;
+
+    __init__ É um construtor, usado para inicializar valores;
+    Inicia um objeto na classe... 
+    É o primeiro a ser executado.  
+
+    __del__ É um destrutor, quando o objeto não está mais 
+    sendo usado, podemos 'destruí-lo' para liberar espaço 
+    na memória;
+    É executado por último, depois de todo processamento;
+    Podemos forçar a destruição chamando a palavra reservada 
+    'del'.
+
+3 Exemplos:
+
+Recursos externos: Se um objeto estiver utilizando recursos
+externos, como arquivos, conexões de rede ou bloqueios, 
+um destrutor pode ser usado para liberar esses recursos 
+quando o objeto não for mais necessário. 
+Por exemplo:
+
+class Arquivo:
+    def __init__(self, nome_arquivo):
+        self.arquivo = open(nome_arquivo, 'r')
+
+    def __del__(self):
+        self.arquivo.close()    
+
+Nesse exemplo, o destrutor fecha o arquivo aberto quando o 
+objeto Arquivo é destruído, garantindo que os recursos sejam 
+liberados adequadamente.
+        
+Logging e depuração: Em situações em que você deseja registrar
+informações quando um objeto é destruído, um destrutor pode 
+ser útil. Isso pode ser útil para fins de depuração e 
+monitoramento. Por exemplo:
+
+import logging
+
+class ObjetoLog:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def __del__(self):
+        logging.info(f'O objeto {self.nome} foi destruído.')
+
+Nesse exemplo, o destrutor registra uma mensagem de log 
+quando o objeto é destruído, permitindo rastrear o ciclo de 
+vida do objeto.
+
+Liberação de recursos personalizados: Se um objeto estiver 
+usando recursos personalizados que não são automaticamente 
+liberados pelo coletor de lixo, um destrutor pode ser usado 
+para realizar a liberação adequada desses recursos. Isso pode 
+ser útil em casos específicos, como quando há uma conexão 
+com um hardware externo ou uma biblioteca que requer 
+liberação EXPLÌCITA de recursos.
+'''
+
+
 class Cachorro:
     def __init__(self, nome, cor, acordado=True):
         print('Inicializando a classe..')
@@ -10,6 +69,9 @@ class Cachorro:
 
     def __del__(self):
         print('Removendo a instância da classe.')
+        # 'pass' -> se não for comentado nada, ele só destroi  
+        # e ninguém fica sabendo
+
     def latir(self):
         print('au au au')
 
@@ -19,11 +81,12 @@ def criar_cachorro():
     print(c.nome)
 
 
-c = Cachorro('Chappie', 'caramelo')
-Cachorro.latir(c)  # c.latir()
-print('Olá mundo')
-del c
-print('Olá mundo')
-print('Olá mundo')
-print('Olá mundo')
 criar_cachorro()
+# c = Cachorro('Chappie', 'caramelo')
+# Cachorro.latir(c)  # c.latir()
+# print('Olá mundo')
+# del c
+# print('Olá mundo')
+# print('Olá mundo')
+# print('Olá mundo')
+# criar_cachorro()
